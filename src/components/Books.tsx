c8fb13ee74cc5e6d1296351d6648913845659522
stefaniahellmann <fani.hell@hotmail.com>
2025-07-28 22:53:34 -0300

    Updated Books.tsx

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -42,8 +42,8 @@ export function Books() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-        {sortedBooks.slice(0, 5).map((book, index) => {
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-6">
+        {sortedBooks.slice(0, 4).map((book, index) => {
           const isUnlocked = books.indexOf(book) === unlockedIndex;
 
           return (
