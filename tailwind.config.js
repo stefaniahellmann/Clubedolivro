6593eed2b61f2a3b4eff4e957f88df7038013f4d
stefaniahellmann <fani.hell@hotmail.com>
2025-08-28 11:52:15 -0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
-// tailwind.config.js
-import typography from '@tailwindcss/typography';
-import aspectRatio from '@tailwindcss/aspect-ratio';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: { extend: {} },
-  plugins: [typography, aspectRatio],
+  plugins: [], // <- sem plugins
 };
