97f0ad6e16cf713f41a4c9f2d51153a5aa4e3936
stefaniahellmann <fani.hell@hotmail.com>
2025-08-27 23:58:27 -0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // habilita tema escuro via classe "dark" na <html>
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
