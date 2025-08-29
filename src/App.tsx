b04762786a4f32bcf1c17c9ab2abf4e5eb3ac9bb
stefaniahellmann <fani.hell@hotmail.com>
2025-08-29 00:57:19 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -57,19 +57,3 @@ export default function App() {
   );
 }
 
-import { ConfigProvider } from './contexts/ConfigContext';
-
-function App() {
-  return (
-    <div className="min-h-screen bg-zinc-50 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-100 transition-colors">
-      <ConfigProvider>
-        <AuthProvider>
-          <AppContent />
-        </AuthProvider>
-      </ConfigProvider>
-      <FloatingThemeToggle />
-    </div>
-  );
-}
-export default App;
-
